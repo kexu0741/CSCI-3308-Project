bee1ee224442fe7bd3bb3cede46f48232a5c7824
slundell123 <salu3240@colorado.edu>
2019-10-21 18:13:53 -0600

    Adding node.js, just a function test

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = function(width, height) {
+    return width * height;
+  };
+
+  //To test in terminal type: node
+  //var area = require('./index.js');
+  // result should be undefined.
+  // now run area(2,4) -> should return 8
